6c94d9c7f875a0ae781074288671bbe2a9cbdcde
Sylom <saberu1213@gmail.com>
2026-01-15 20:13:03 +0800

    deletion

--- web/firebase-messaging-sw.js
+++
@@ -1,8 +0,0 @@
-importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
-
-firebase.initializeApp({
-  // Your Firebase config
-});
-
-const messaging = firebase.messaging();
